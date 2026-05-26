614c8e82ecee68a8afd4633560ab51ec1979cb87
abdulrahman <abdulrahmanhamza270@gmail.com>
2026-05-26 22:32:26 +0100

    updated compass_screen

--- lib/app/features/screens/compass_screen.dart
+++ lib/app/features/screens/compass_screen.dart
@@ -19,6 +19,7 @@ class CompassScreen extends StatefulWidget {
 
 class _CompassScreenState extends State<CompassScreen> {
   StreamSubscription<CompassEvent>? _compassSub;
+  Position? _position;
 
   double _deviceHeading = 0;
   double _qiblaBearing = 0;
@@ -60,6 +61,7 @@ class _CompassScreenState extends State<CompassScreen> {
         pos.longitude,
       );
       setState(() {
+        _position = pos;
         _qiblaBearing = bearing;
         _distanceKm = distance;
         _isLoadingLocation = false;
